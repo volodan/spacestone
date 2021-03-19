29dd027d62020d42bde7f717581ff404ffb0a4ad
vnu7508 <vladislav.jevcak.ext@uniqa.at>
2021-03-19 10:47:50 +0100

    small refactoring

--- src/main/java/com/uniqa/spacestone/SpacestoneApplication.java
+++ src/main/java/com/uniqa/spacestone/SpacestoneApplication.java
@@ -2,6 +2,9 @@ package com.uniqa.spacestone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import net.sourceforge.tess4j.Tesseract;
 
 @SpringBootApplication
 public class SpacestoneApplication {
@@ -10,4 +13,18 @@ public class SpacestoneApplication {
 		SpringApplication.run(SpacestoneApplication.class, args);
 	}
 
+	@Bean
+	public Tesseract getTesseract() {
+		Tesseract tesseract = new Tesseract(); 
+
+		// FIXME - can be set as an absolute path
+		// set path to tess learned data for OCR
+        tesseract.setDatapath(System.getenv("TESSDATA_PREFIX")); // this was set in OS
+        
+		// set language and psm for page segmentation, 3 means, that page contains more lines
+        tesseract.setPageSegMode(3);
+        tesseract.setLanguage("deu");
+
+		return tesseract;
+	} 
 }
--- src/main/java/com/uniqa/spacestone/controller/OcrController.java
+++ src/main/java/com/uniqa/spacestone/controller/OcrController.java
@@ -21,6 +21,7 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import com.uniqa.spacestone.dto.FuzzySearchResultDto;
@@ -34,47 +35,49 @@ public class OcrController {
     @Autowired
     private TextProcessorService textProcessorService;
 
-    @PostMapping
-    public ResponseEntity<List<FuzzySearchResultDto>> ocrImage(@RequestParam String language, @RequestParam List<String> queryStrings,
+    @Autowired
+    private Tesseract tesseract;
+
+    @PostMapping(value = "/fuzzySearch")
+    public ResponseEntity<List<FuzzySearchResultDto>> ocrImage(@RequestParam List<String> queryStrings,
         @RequestPart("document") MultipartFile document) {
 
         List<FuzzySearchResultDto> result = new ArrayList<>();    
-        Tesseract tesseract = new Tesseract(); 
-        
-        try { 
-  
-            // set path to tess learned data for OCR
-            tesseract.setDatapath(System.getenv("TESSDATA_PREFIX")); // this was set in OS
-            // set config map l is for language and psm for page segmentation, 3 means, that page contains more lines
-            tesseract.setPageSegMode(3);
-            tesseract.setLanguage(language);
 
+        processPdfDocument(document, (pageText) -> {
+
+            List<String> wordsInPageText = Arrays.asList(pageText.split("\\s+"));
+            // remove all empty places
+            wordsInPageText.forEach(item -> item.trim());
+
+            List<String> listExtResult = new ArrayList<>();
+            // now do fuzzy search for every query string in the page
+            for (String queryString : queryStrings) {
+                // also remove strings which have -2
+                List<ExtractedResult> tempRes = textProcessorService.fuzzyStringSearch(queryString, 
+                    wordsInPageText.stream().filter(word -> (StringUtils.hasText(word) && word.length() > queryString.length() - 2 )).collect(Collectors.toList()));
+                listExtResult.add(queryString + " => " + tempRes.toString());
+            }
+            result.add(FuzzySearchResultDto.builder().searchResult(listExtResult).ocrText(pageText).build());
+        });
+
+        return ResponseEntity.ok().body(result);
+    }
+
+    private void processPdfDocument(MultipartFile document, Consumer<String> pageConsumer) {
+        try { 
             // convert PDF to jpeg
             PDDocument pdfDoc = PDDocument.load(document.getInputStream());
             PDFRenderer pdfRenderer = new PDFRenderer(pdfDoc);
 
-            // for every page on doc, render to image and then OCR
             for (int page = 0; page < pdfDoc.getNumberOfPages(); page++) {
                 BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(page, 96, ImageType.GRAY);
                 String pageText = tesseract.doOCR(bufferedImage);
-                List<String> wordsInPageText = Arrays.asList(pageText.split("\\s+"));
-                // remove all empty places
-                wordsInPageText.forEach(item -> item.trim());
-
-                List<String> listExtResult = new ArrayList<>();
-                // now do fuzzy search for every query string in the page
-                for (String queryString : queryStrings) {
-                    // also remove strings which have -2
-                    List<ExtractedResult> tempRes = textProcessorService.fuzzyStringSearch(queryString, 
-                        wordsInPageText.stream().filter(word -> (StringUtils.hasText(word) && word.length() > queryString.length() - 2 )).collect(Collectors.toList()));
-                    listExtResult.add(queryString + " => " + tempRes.toString());
-                }
-                result.add(FuzzySearchResultDto.builder().searchResult(listExtResult).ocrText(pageText).build());
+                pageConsumer.accept(pageText);
             }
-        } 
-        catch (Exception e) { 
+    
+        } catch (Exception e) { 
             log.error("Exception occured during processing file: {}", document.getOriginalFilename(), e);
         }
-        return ResponseEntity.ok().body(result);
     }
 }
